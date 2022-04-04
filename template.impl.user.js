08febdb90a2255cdf8d4096f9179761d19fedab1
melitonsi <66038513+melitonsi@users.noreply.github.com>
2022-04-04 02:05:04 -0400

    Create template.impl.user.js

---
+++ template.impl.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Burssty Place Template
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the canvas!
+// @author       avery307
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/melitonsi/burssty-place/main/bursstyplace2.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
